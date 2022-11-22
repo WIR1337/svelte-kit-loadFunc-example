f1ad2a383b21c2453ec9f4cf8bcf41f384ff2f1e
Alexandr N <naberezhnyy.an@gmail.com>
2022-11-22 10:07:48 +0100

    asdf

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p><a href="/blog/1">Blog 1</a></p>
+<p><a href="/blog/2">Blog 2</a></p>
+<p><a href="/blog/3">Blog 3</a></p>
