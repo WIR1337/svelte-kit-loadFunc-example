7098c789819cf7ad833c2f19b749c3940ceaa7c0
Alexandr N <naberezhnyy.an@gmail.com>
2022-11-22 13:45:15 +0100

    final

--- src/routes/blog/[id]/+page.js
+++ src/routes/blog/[id]/+page.js
@@ -1,6 +1,4 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ params, url }) {
-    let lang = url.searchParams.get('lang');
-    let q = url.searchParams.get('q');
     return { id: params.id };
 }
